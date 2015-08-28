ca7bd279dc70015ca47a0bfc7bdb597dcca2ead2
Amy Thompson <amytgarcia@gmail.com>
2015-08-27 21:24:31 -0500

    Add javascript name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ //$("#main").append("Amy Thompson");
+
+var firstName = "awesomeThoughts";
+var awesomeThoughts="I am Amy and I am AWESOME!"
+console.log(awesomeThoughts);
+
+var funThoughts =
+	awesomeThoughts.replace("AWESOME","FUN");
+
+$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Amy Thompson");
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Systems Administrator")
+$("#header").append(formattedRole);
