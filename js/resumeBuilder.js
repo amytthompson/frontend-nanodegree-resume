0d8d65f63ccc3124eb55cb9216b5cc2ffcbcdabe
Amy Thompson <amytgarcia@gmail.com>
2015-09-06 14:51:24 -0500

    Style: removed instructor notes and examples

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,22 +1,31 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
- //$("#main").append("Amy Thompson");
-
-var firstName = "awesomeThoughts";
-var awesomeThoughts="I am Amy and I am AWESOME!"
-console.log(awesomeThoughts);
-
-var funThoughts =
-	awesomeThoughts.replace("AWESOME","FUN");
-
 var name = "Amy Thompson";
 var formattedName = HTMLheaderName.replace("%data%", name);
 
 var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role)
+var formattedRole = HTMLheaderRole.rep
 
-$("#main").append(funThoughts);
+var skills = ["awesomeness", "programming", "teaching", "JS"];
+
+var bio = {
+	"name" : name,
+	"role" : role,
+	"contact info" : {
+		"email" : "amityatx@gmail.com",
+		"github" : "github",
+		"twitter" : "@amytgarcia",
+		"location" : "austintx",
+	},
+	"welcome message" : "Welcome!",
+	"skills" : skills : ["reading","writing", "rithmatic"],
+	"biopic" : "images/me.jpg",
+};
+
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#main").append(skills);
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio."contact info");
+$("#main").append(bio."picture URL");
+$("#main").append(bio."welcome message");
+$("#main").append(bio."skills");
